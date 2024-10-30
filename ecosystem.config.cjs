0c9e79697fabfc84ff8465528c3975921662b6e0
lijunqin <lijunqin@Mac-mini.local>
2024-10-30 15:35:05 +0800

    feat: add pm2 deploy config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,8 @@
+//!!You need to close standalone output on next.config.mjs first
 module.exports = {
   apps: [
     {
-      name: "302AI-AI-Tool",
+      name: "302_AI_Tool",
       script: "./node_modules/next/dist/bin/next",
       args: "start -p 3000",
       env: {
