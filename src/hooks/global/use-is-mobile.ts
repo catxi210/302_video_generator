a1c1bcd741b3e0c0355ff19509d8a214bcc9c0f3
lijunqin <lijunqin@Mac-mini.local>
2024-10-30 14:30:16 +0800

    style: format hooks code

--- src/hooks/global/use-is-mobile.ts
+++ src/hooks/global/use-is-mobile.ts
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 
-// 定义一个阈值，当窗口宽度小于这个值时，认为是移动设备
 const MOBILE_MAX_WIDTH = 600;
 
 export const useIsMobile = () => {
@@ -15,10 +14,8 @@ export const useIsMobile = () => {
       setIsMobile(window.innerWidth < MOBILE_MAX_WIDTH);
     };
 
-    // 监听窗口尺寸变化事件
     window.addEventListener("resize", handleResize);
 
-    // 组件卸载时移除事件监听器
     return () => {
       window.removeEventListener("resize", handleResize);
     };
